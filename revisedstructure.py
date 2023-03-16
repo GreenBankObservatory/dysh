be540be08b7b79157d01eab579d5a9011fbc8e5a
Marc Pound <22331890+mpound@users.noreply.github.com>
2023-03-16 15:23:55 -0400

    update to get stats for multiple HDUs

--- revisedstructure.py
+++ revisedstructure.py
@@ -40,6 +40,7 @@ if __name__ == "__main__":
     nrows = np.zeros(len(files))
     nchanl = np.zeros(len(files))
     t0 = np.zeros(len(files))
+    t0a = np.zeros(len(files))
     t1 = np.zeros(len(files))
     t2 = np.zeros(len(files))
     t3 = np.zeros(len(files))
@@ -47,7 +48,13 @@ if __name__ == "__main__":
     tb2 = np.zeros(len(files))
     tb3 = np.zeros(len(files))
     t5 = np.zeros(len(files))
+    hdu = np.zeros(len(files))
     i=0
+    names =['Size','N_hdu','HDU','N_rows','N_chan', 'Load','Create_Obsblocks','Baseline_1', 'Baseline_2', 'Baseline_3', 'Total']
+    units = ['MB',   '',    '',    '',    '' ,      'ms',      'ms',          'ms',            'ms'      ,   'ms',          'ms' ]
+    dtypes = [float, int,    int, int,   int,      float,     float,            float,          float,         float,        float]
+    table = Table( names=names, meta={'name':'SDFITSLoad Timing'},
+                   units=units,dtype=dtypes)
     for fn in files:
         pr = cProfile.Profile()
         inf = f'{examples}{fn}'
@@ -55,38 +62,46 @@ if __name__ == "__main__":
         t0[i] = time.perf_counter_ns()
         pr.enable()
         s = SDFITSLoad(fn)
-        t1[i] = time.perf_counter_ns()
-        s._loadlists(fix=args.fix,wcs=args.wcs)
-        s._hdu.close()
-        t2[i] = time.perf_counter_ns()
-        nchanl[i] = len(s._obsblock[0][0].spectral_axis)
-        if args.baseline:
-            j=0
-            for o in s._obsblock:
-                p = o[0]
-                x = o[0].spectral_axis
-                nchan = len(x)
-                print(f"DIFF for HDU {j} {nchanl[i]-nchan}",file=sys.stderr)
-                cdelt = x[1]-x[0]
-                center = x[nchan // 2]
-                width = 0.25*nchan*cdelt
-                exclude = SpectralRegion.from_center(center,width)
-                print("EXCLUDE ",exclude,file=sys.stderr)
-                baseline(speclist=o,order=1,exclude=exclude,maxspec=1000)
-                tb1[i] = time.perf_counter_ns()
-                print("Done O1",file=sys.stderr)
-                baseline(speclist=o,order=2,exclude=exclude,maxspec=1000)
-                tb2[i] = time.perf_counter_ns()
-                print("Done O2",file=sys.stderr)
-                baseline(speclist=o,order=3,exclude=exclude,maxspec=1000)
-                tb3[i] = time.perf_counter_ns()
-                print("Done O3",file=sys.stderr)
-                j=j+1
-        t3[i] = time.perf_counter_ns()
-        pr.disable()
-        #s.summary()
-        nhdu[i] = len(s._hdu)
-        nrows[i] = np.sum(s._nrows)
+        t0a[i] = time.perf_counter_ns()
+        for h in range(1,len(s._hdu)):
+            hdu[i] = h
+            t1[i] = time.perf_counter_ns()
+            s._loadlists(fix=args.fix,wcs=args.wcs,hdu=int(hdu))
+            t2[i] = time.perf_counter_ns()
+            nchanl[i] = len(s._obsblock[0][0].spectral_axis)
+            if args.baseline:
+                j=0
+                for o in s._obsblock:
+                    p = o[0]
+                    x = o[0].spectral_axis
+                    nchan = len(x)
+                    cdelt = x[1]-x[0]
+                    center = x[nchan // 2]
+                    width = 0.25*nchan*cdelt
+                    exclude = SpectralRegion.from_center(center,width)
+                    print("EXCLUDE ",exclude,file=sys.stderr)
+                    baseline(speclist=o,order=1,exclude=exclude,maxspec=1000)
+                    tb1[i] = time.perf_counter_ns()
+                    print("Done O1",file=sys.stderr)
+                    baseline(speclist=o,order=2,exclude=exclude,maxspec=1000)
+                    tb2[i] = time.perf_counter_ns()
+                    print("Done O2",file=sys.stderr)
+                    baseline(speclist=o,order=3,exclude=exclude,maxspec=1000)
+                    tb3[i] = time.perf_counter_ns()
+                    print("Done O3",file=sys.stderr)
+                    j=j+1
+            t3[i] = time.perf_counter_ns()
+            pr.disable()
+            #s.summary()
+            nhdu[i] = len(s._hdu)-1
+            nrows[i] = s.nrows(h-1)
+            load = (t0-t0a)/1E6
+            obs  = (t2-t1)/1E6
+            base1  = (tb1-t2)/1E6
+            base2  = (tb2-tb1)/1E6
+            base3  = (tb3-tb2)/1E6
+            total = (t3-t0)/1E6
+            table.add_row([size,nhdu,hdu,nrows,nchanl,load,obs,base1,base2,base3,total])
         del s
 
         #s = io.StringIO()
@@ -95,18 +110,7 @@ if __name__ == "__main__":
         ps.print_stats(25)
         i+=1
 
-    load = (t1-t0)/1E6
-    obs  = (t2-t1)/1E6
-    base1  = (tb1-t2)/1E6
-    base2  = (tb2-tb1)/1E6
-    base3  = (tb3-tb2)/1E6
-    total = (t3-t0)/1E6
-    names =['Size','N_hdu','N_rows','N_chan', 'Load','Create_Obsblocks','Baseline_1', 'Baseline_2', 'Baseline_3', 'Total']
-    units = ['MB',   '',      '',    '' ,      'ms',      'ms',          'ms',            'ms'      ,   'ms',          'ms' ]
-    dtypes = [float, int,    int,    int,      float,     float,            float,          float,         float,        float]
-    table = Table(data=[size,nhdu,nrows,nchanl,load,obs,base1,base2,base3,total],
-                   names=names, meta={'name':'SDFITSLoad Timing'},
-                   units=units,dtype=dtypes)
+
     for c in table.columns:
         if table[c].info.dtype == float:
             table[c].info.format = '0.1f'
