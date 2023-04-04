0f807c453b58972c2c2a843a8cda817d5b252379
Marc Pound <22331890+mpound@users.noreply.github.com>
2023-04-04 09:30:44 -0400

    add more args and table columns

--- revisedstructure.py
+++ revisedstructure.py
@@ -13,11 +13,13 @@ import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="revisedstructure")
-    parser.add_argument('--file','-f', action='store', help='input filename')
+    parser.add_argument('--file','-f', action='store', help='input filename', required=True)
     parser.add_argument('--wcs',action='store_true', help='create WCS for each Spectrum1D')
     parser.add_argument('--fix',action='store_true', help='fix warnings about keywords in WCS creation')
     parser.add_argument('--profile', '-p',  action='store_true', help='run the profiler')
     parser.add_argument('--baseline','-b', action='store_true', help='remove baselines')
+    parser.add_argument('--maxload','-m', action='store', help='maximum number of spectra to load (create obsblocks)',default=1E16)
+#    parser.add_argument('--baseline','-b', action='store', help='remove baselines',default=None)
     args = parser.parse_args()
     print(f"using {args}")
 
@@ -50,12 +52,13 @@ if __name__ == "__main__":
     t5 = np.zeros(len(files))
     hdu = np.zeros(len(files))
     i=0
-    names =['Size','N_hdu','HDU','N_rows','N_chan', 'Load','Create_Obsblocks','Baseline_1', 'Baseline_2', 'Baseline_3', 'Total']
-    units = ['MB',   '',    '',    '',    '' ,      'ms',      'ms',          'ms',            'ms'      ,   'ms',          'ms' ]
-    dtypes = [float, int,    int, int,   int,      float,     float,            float,          float,         float,        float]
+    names =['File', 'Size','N_hdu','HDU','N_rows','N_chan', 'Load','Create_Obsblocks','Baseline_1', 'Baseline_2', 'Baseline_3', 'Total']
+    units = ['', 'MB',   '',    '',    '',    '' ,      'ms',      'ms',          'ms',            'ms'      ,   'ms',          'ms' ]
+    dtypes = [str, float, int,    int, int,   int,      float,     float,            float,          float,         float,        float]
     table = Table( names=names, meta={'name':'SDFITSLoad Timing'},
                    units=units,dtype=dtypes)
     for fn in files:
+        basename = os.path.basename(fn)
         pr = cProfile.Profile()
         inf = f'{examples}{fn}'
         size[i] = os.path.getsize(fn)/1048576
@@ -66,10 +69,10 @@ if __name__ == "__main__":
         for h in range(1,len(s._hdu)):
             hdu[i] = h
             t1[i] = time.perf_counter_ns()
-            s._loadlists(fix=args.fix,wcs=args.wcs,hdu=int(hdu))
+            s._loadlists(fix=args.fix,wcs=args.wcs,hdu=int(hdu),maxspect=float(args.maxload))
             t2[i] = time.perf_counter_ns()
             nchanl[i] = len(s._obsblock[0][0].spectral_axis)
-            if args.baseline:
+            if args.baseline:# is not None:
                 j=0
                 for o in s._obsblock:
                     p = o[0]
@@ -101,7 +104,7 @@ if __name__ == "__main__":
             base2  = (tb2-tb1)/1E6
             base3  = (tb3-tb2)/1E6
             total = (t3-t0)/1E6
-            table.add_row([size,nhdu,hdu,nrows,nchanl,load,obs,base1,base2,base3,total])
+            table.add_row([basename,size,nhdu,hdu,nrows,nchanl,load,obs,base1,base2,base3,total])
         del s
 
         #s = io.StringIO()
