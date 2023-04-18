f3fb0f0c3e3b499e405c3796c92362cf2b894d1c
Marc Pound <22331890+mpound@users.noreply.github.com>
2023-04-18 16:45:06 -0400

    add index option

--- benchmark/revisedstructure.py
+++ benchmark/revisedstructure.py
@@ -18,6 +18,8 @@ if __name__ == "__main__":
     parser.add_argument('--fix',action='store_true', help='fix warnings about keywords in WCS creation')
     parser.add_argument('--profile', '-p',  action='store_true', help='run the profiler')
     parser.add_argument('--baseline','-b', action='store_true', help='remove baselines')
+    parser.add_argument('--obsblocks','-o', action='store_true', help='create obsblocks')
+    parser.add_argument('--pandas','-i', action='store_true', help='create pandas table (index)')
     parser.add_argument('--maxload','-m', action='store', help='maximum number of spectra to load (create obsblocks)',default=1E16)
 #    parser.add_argument('--baseline','-b', action='store', help='remove baselines',default=None)
     args = parser.parse_args()
@@ -44,7 +46,9 @@ if __name__ == "__main__":
     t0 = np.zeros(len(files))
     t0a = np.zeros(len(files))
     t1 = np.zeros(len(files))
+    t1p = np.zeros(len(files))
     t2 = np.zeros(len(files))
+    t2p = np.zeros(len(files))
     t3 = np.zeros(len(files))
     tb1 = np.zeros(len(files))
     tb2 = np.zeros(len(files))
@@ -52,9 +56,9 @@ if __name__ == "__main__":
     t5 = np.zeros(len(files))
     hdu = np.zeros(len(files))
     i=0
-    names =['File', 'Size','N_hdu','HDU','N_rows','N_chan', 'Load','Create_Obsblocks','Baseline_1', 'Baseline_2', 'Baseline_3', 'Total']
-    units = ['', 'MB',   '',    '',    '',    '' ,      'ms',      'ms',          'ms',            'ms'      ,   'ms',          'ms' ]
-    dtypes = [str, float, int,    int, int,   int,      float,     float,            float,          float,         float,        float]
+    names =['File', 'Size','N_hdu','HDU','N_rows','N_chan', 'Load','Index', 'Create_Obsblocks','Baseline_1', 'Baseline_2', 'Baseline_3', 'Total']
+    units = ['',     'MB',   '',    '',    '',      '' ,    'ms',   'ms',      'ms',             'ms',            'ms'      ,   'ms',       'ms' ]
+    dtypes = [str, float,   int,    int,  int,    int,      float,  float,    float,            float,          float,         float,      float]
     table = Table( names=names, meta={'name':'SDFITSLoad Timing'},
                    units=units,dtype=dtypes)
     for fn in files:
@@ -66,12 +70,18 @@ if __name__ == "__main__":
         pr.enable()
         s = SDFITSLoad(fn)
         t0a[i] = time.perf_counter_ns()
+        if args.pandas:
+            for h in range(1,len(s._hdu)):
+                t1p[i] = time.perf_counter_ns()
+                s.load_pandas(hdu=h)
+                t2p[i] = time.perf_counter_ns()
         for h in range(1,len(s._hdu)):
             hdu[i] = h
-            t1[i] = time.perf_counter_ns()
-            s._loadlists(fix=args.fix,wcs=args.wcs,hdu=int(hdu),maxspect=float(args.maxload))
-            t2[i] = time.perf_counter_ns()
-            nchanl[i] = len(s._obsblock[0][0].spectral_axis)
+            if args.obsblocks:
+                t1[i] = time.perf_counter_ns()
+                s._loadlists(fix=args.fix,wcs=args.wcs,hdu=int(hdu),maxspect=float(args.maxload))
+                t2[i] = time.perf_counter_ns()
+                nchanl[i] = len(s._obsblock[0][0].spectral_axis)
             if args.baseline:# is not None:
                 j=0
                 for o in s._obsblock:
@@ -99,12 +109,13 @@ if __name__ == "__main__":
             nhdu[i] = len(s._hdu)-1
             nrows[i] = s.nrows(h-1)
             load = (t0a-t0)/1E6
+            pandas = (t2p-t1p)/1E6
             obs  = (t2-t1)/1E6
             base1  = (tb1-t2)/1E6
             base2  = (tb2-tb1)/1E6
             base3  = (tb3-tb2)/1E6
             total = (t3-t0)/1E6
-            table.add_row([basename,size,nhdu,hdu,nrows,nchanl,load,obs,base1,base2,base3,total])
+            table.add_row([basename,size,nhdu,hdu,nrows,nchanl,load,pandas,obs,base1,base2,base3,total])
         del s
 
         #s = io.StringIO()
