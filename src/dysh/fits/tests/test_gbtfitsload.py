f74bc3a4b9a6258f52b7dfe0e043b558a186341a
astrofle <psalas@nrao.edu>
2023-07-03 17:21:36 -0400

    Passing getps test! Kudos to the team

--- src/dysh/fits/tests/test_gbtfitsload.py
+++ src/dysh/fits/tests/test_gbtfitsload.py
@@ -1,8 +1,11 @@
 
 import os
 import pytest
+import numpy as np
 
+from astropy.io import fits
 from astropy.utils.data import (
+                    get_pkg_data_filename,
                     get_pkg_data_filenames,
                     )
 
@@ -29,3 +32,23 @@ class TestGBTFITSLoad():
             sdf = gbtfitsload.GBTFITSLoad(fnm)
             assert len(sdf._ptable[0]) == expected[filename]
 
+
+    def test_getps(self):
+        """
+        """
+
+        gbtidl_file = get_pkg_data_filename("data/TGBT21A_501_11_getps_scan_152_intnum_0_ifnum_0_plnum_0.fits")
+        # We should probably use dysh to open the file...
+        hdu = fits.open(gbtidl_file)
+        gbtidl_getps = hdu[1].data["DATA"][0]
+
+        sdf_file = get_pkg_data_filename("data/TGBT21A_501_11.raw.vegas.fits")
+        sdf = gbtfitsload.GBTFITSLoad(sdf_file)
+        psscan = sdf.getps(152)
+        psscan.calibrate()
+        dysh_getps = psscan.calibrated(0).flux.to("K").value
+
+        diff = gbtidl_getps - dysh_getps
+        assert np.nanmedian(diff) == 0.0
+        assert np.all(abs(diff[~np.isnan(diff)]) < 5e-7)
+        assert np.isnan(diff[3072])
