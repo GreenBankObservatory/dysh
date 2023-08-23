a21f15bc5202ac8895e130d58d8658f200cff08e
vcatlett-nrao <vcatlett@nrao.edu>
2023-08-23 17:28:45 -0400

    Fixed conf.py paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('../../src'))
+print("PATH HERE: ",sys.path)
+#sys.path.insert(0, os.path.abspath('.'))
 
 import dysh
 # -- Project information -----------------------------------------------------
