f7cf746c7e9031b485a94d9a60544c70192e5d26
vcatlett-nrao <vcatlett@nrao.edu>
2023-12-06 19:29:14 -0500

    Add a test for docstring coverage

---
+++ src/dysh/tests/test_docstr.py
@@ -0,0 +1,12 @@
+from docstr_coverage import analyze
+from glob import glob
+
+def test_docstring_coverage():
+    """
+    Checks that the percentage of Python functions with docstrings 
+    is >= 0%
+    """
+    pyfiles = glob('./**/*.py', recursive = True)
+    coverage_report = analyze(pyfiles)
+    coverage = coverage_report.count_aggregate().coverage()
+    assert coverage >= 0
