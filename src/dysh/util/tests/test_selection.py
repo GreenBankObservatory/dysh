7755c4c3c4c67634d07f5be2d63f3b1b3cdf2d74
Marc Pound <22331890+mpound@users.noreply.github.com>
2024-02-29 17:31:04 -0500

    first version of testing selection class. need to fix selection.remove

---
+++ src/dysh/util/tests/test_selection.py
@@ -0,0 +1,65 @@
+import pathlib
+
+import astropy.units as u
+import pytest
+
+import dysh
+from dysh import util
+from dysh.fits import gbtfitsload
+from dysh.util.selection import Selection
+
+dysh_root = pathlib.Path(dysh.__file__).parent.resolve()
+
+
+class TestSelection:
+    """Test data selection"""
+
+    def setup_method(self):
+        self.file = util.get_project_testdata() / "TGBT21A_501_11/testselection.fits"
+
+    def test_selection_class(self):
+        """
+        Test that the Selection class selects as expected.
+        We check that on each selection, the number of selected rows
+        matches the known correct answer.
+        """
+        sdf = gbtfitsload.GBTFITSLoad(self.file)
+        s = Selection(sdf)
+        # the unfilter sdfits index should be the same as
+        # the default selection
+        assert sdf._index.equals(s)
+        # polarization 0 and source name
+        s.select(object="NGC2415", plnum=0)
+        assert len(s._selection_rules[0]) == 5
+        # Test that a duplicate selection results
+        # in an exception.  Note here, we are
+        # also testing that the alias 'pol' is interpreted
+        # as plnum.
+        with pytest.raises(Exception):
+            s.select(object="NGC2415", pol=0, tag="this will except")
+        s.select(ifnum=[0, 2], tag="ifnums")
+        assert len(s._selection_rules[1]) == 26
+        # the OR of the selection rules becomes the final
+        # selection
+        assert len(s.final) == 3
+        # test s.remove by both id and tag
+        # s.remove(0)
+        # s.remove(tag="ifnums")
+        s = Selection(sdf)
+        # test select_range
+        # lower limit.
+        # RAs range from 53.24 to 247.04 degrees
+        # Decs range from -15.64 to 54.57 degrees
+        s.select_range(ra=(114,))  # default is degrees
+        assert len(s.final) == 40
+        # check that quantities work
+        s.select_range(dec=[2400, 7500] * u.arcmin)
+        assert len(s.final) == 20
+
+        # test select_within
+        s = Selection(sdf)
+        # also verify that the selection variable name is
+        # case insensitive
+        # also note we aliased elevation for elevatio!
+        s.select_within(eLEVaTIon=(18.0, 2))
+        assert len(s.final) == 13
