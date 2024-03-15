b068df2c68042a3d154818e8a4a0cb9dc0820114
astrofle <psalas@nrao.edu>
2024-03-15 19:37:44 -0400

    Adds tests for flux values

--- src/dysh/spectra/tests/test_spectrum.py
+++ src/dysh/spectra/tests/test_spectrum.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from dysh.fits.gbtfitsload import GBTFITSLoad
 from dysh.util import get_project_testdata
 
@@ -18,29 +20,37 @@ class TestSpectrum:
         addition = self.ps0 + self.ps1
 
         assert addition.meta["EXPOSURE"] == (self.ps0.meta["EXPOSURE"] + self.ps1.meta["EXPOSURE"])
+        assert np.all(addition.flux.value == (self.ps0.flux.value + self.ps1.flux.value))
 
     def test_add_scalar(self):
         """Test that we can add a scalar to a `Spectrum`."""
         addition = self.ps0 + 10.0
 
         assert addition.meta["EXPOSURE"] == self.ps0.meta["EXPOSURE"]
+        assert np.all(addition.flux.value == (self.ps0.flux.value + 10.0))
 
     def test_sub(self):
         """Test that we can subtract two `Spectrum`."""
         subtraction = self.ps0 - self.ps1
 
         assert subtraction.meta["EXPOSURE"] == (self.ps0.meta["EXPOSURE"] + self.ps1.meta["EXPOSURE"])
+        assert np.all(subtraction.flux.value == (self.ps0.flux.value - self.ps1.flux.value))
 
     def test_sub_scalar(self):
         """Test that we can subtract a scalar from a `Spectrum`."""
-        addition = self.ps0 - 10.0
+        subtraction = self.ps0 - 10.0
 
-        assert addition.meta["EXPOSURE"] == self.ps0.meta["EXPOSURE"]
+        assert subtraction.meta["EXPOSURE"] == self.ps0.meta["EXPOSURE"]
+        assert np.all(subtraction.flux.value == (self.ps0.flux.value - 10.0))
 
     def test_mul(self):
         """Test that we can multiply two `Spectrum`."""
         multiplication = self.ps0 * self.ps1
 
+        assert np.all(multiplication.flux.value == (self.ps0.flux.value * self.ps1.flux.value))
+
     def test_div(self):
         """Test that we can divide two `Spectrum`."""
         division = self.ps0 / self.ps1
+
+        assert np.all(division.flux.value == (self.ps0.flux.value / self.ps1.flux.value))
