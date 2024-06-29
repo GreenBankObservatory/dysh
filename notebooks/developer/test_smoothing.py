8c23366b1c0375737d0d23fc5a17d8786d7250cb
Peter Teuben <teuben@gmail.com>
2024-06-29 08:22:09 -0400

    cleanup of old tests, confirm that with a odd box size IDL and DYSH agree.

--- notebooks/developer/test_smoothing.py
+++ notebooks/developer/test_smoothing.py
@@ -25,7 +25,7 @@ def parr(data, n, w=1):
     print(data[n-w:n+w+1])
     
     
-#%%  testing simple spectra
+#%%  testing simple spectra; but we're not using specutils anymore
 
 spec1 = Spectrum1D(spectral_axis=np.arange(1, 50) * u.nm, flux=np.random.default_rng(12345).random(49)*u.Jy)
 
@@ -50,35 +50,28 @@ import dysh.util as util
 from dysh.util.selection import Selection
 
 f1 = util.get_project_testdata() / 'TGBT21A_501_11/TGBT21A_501_11.raw.vegas.fits'
-s1 = SDFITSLoad(f1)
+# s1 = SDFITSLoad(f1)
 
 sdf1 = GBTFITSLoad(f1)
 sdf1.info()
 sdf1.summary(verbose=True)
 
-p1 = sdf1.getps(scan=152, ifnum=0, plnum=0, calibrate=True)
-
+p1 = sdf1.getps(scan=152, ifnum=0, plnum=0)
 sp1 = p1[0].calibrated(0)
-if False:
-    sp1.flux.value[1] = 0
-    sp1.flux.value[2] = 1
-    sp1.flux.value[3] = 0
-if False:
-    nchan = len(sp1.flux.value)
-    sp1.flux.value[5000] = 1000
     
-sp1b = sp1.smooth("boxcar",5)
-sp1h = sp1.smooth("hanning")
-sp1g = sp1.smooth("gaussian",5)
+sp1b = sp1.smooth("boxcar",5,-1)
+sp1h = sp1.smooth("hanning",1,-1)
+sp1g = sp1.smooth("gaussian",5,-1)
 
 d1=sp1.flux.value
 d1b=sp1b.flux.value
 d1h=sp1h.flux.value
 d1g=sp1g.flux.value
 
-# now the smoothref version
+# now the smoothref version; it needs to be odd, otherwise
+# the IDL kernel isn't the same as astropy
 
-p1s = sdf1.getps(scan=152, ifnum=0, plnum=0, calibrate=True, smoothref=32)
+p1s = sdf1.getps(scan=152, ifnum=0, plnum=0, smoothref=15)
 sp1s = p1s[0].calibrated(0)
 d1s = sp1s.flux.value
 
@@ -94,11 +87,11 @@ keep
 """
 
 base =  'TGBT21A_501_11/TGBT21A_501_11_getps_scan_152_intnum_0_ifnum_0_plnum_0%s.fits'
-base1 = base % ''
+base1  = base % ''
 base1b = base % '_boxcar_5'
 base1h = base % '_hanning'
 base1g = base % '_gsmooth_5'
-base1s = base % '_smthoff_32'
+base1s = base % '_smthoff_15'
 
 d2  = fits.open(util.get_project_testdata() / base1)[1].data['DATA'][0]
 d2b = fits.open(util.get_project_testdata() / base1b)[1].data['DATA'][0]
@@ -107,15 +100,17 @@ d2g = fits.open(util.get_project_testdata() / base1g)[1].data['DATA'][0]
 d2s = fits.open(util.get_project_testdata() / base1s)[1].data['DATA'][0]
 
 
-e2 = d1-d2
+e2  = d1-d2
 e2b = (d1b-d2b)
 e2h = (d1h-d2h)
 e2g = (d1g-d2g)
 e2s = (d1s-d2s)
 
-print("d1-d2:", np.nanstd(d1-d2))     # 3e-8
-print("e box:", np.nanstd(e2b))  
-print("e han:", np.nanstd(e2h))    
-print("e gau:", np.nanstd(e2g))
+print("d1    :", np.nanstd(d1))
+print("d1-d2 :", np.nanstd(d1-d2)) 
+print("e box :", np.nanstd(e2b))  
+print("e han :", np.nanstd(e2h))    
+print("e gau :", np.nanstd(e2g))
 print("e smth:", np.nanstd(e2s))
+
 
