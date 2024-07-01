9b8617e77cabf0957658b49302ba5143891f8f13
Marc Pound <22331890+mpound@users.noreply.github.com>
2024-07-01 08:59:31 -0700

    fix  local variable 'k' referenced before assignment

--- src/dysh/fits/gbtfitsload.py
+++ src/dysh/fits/gbtfitsload.py
@@ -1742,7 +1742,7 @@ class GBTFITSLoad(SDFITSLoad):
             if verbose:
                 print(f"Total of {total_rows_written} rows written to files.")
         else:
-            hdu = self._sdf[k]._hdu[fi[0]].copy()
+            hdu = self._sdf[fi[0]]._hdu[fi[0]].copy()
             outhdu = fits.HDUList(hdu)
             for k in fi:
                 df = select_from("FITSINDEX", k, _final)
