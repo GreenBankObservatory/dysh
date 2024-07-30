7f4386a4587469cac60bca4ba0042299be5373cf
vcatlett-nrao <vcatlett@nrao.edu>
2024-07-30 13:20:32 -0400

    compile requirements.txt with Python 3.9

--- src/dysh/plot/iPlotter.py
+++ src/dysh/plot/iPlotter.py
@@ -4,9 +4,9 @@ import time
 
 import pyqtgraph as pg
 from IPython.display import display
-from PyQt6.QtCore import *
-from PyQt6.QtGui import *
-from PyQt6.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 from pyqtgraph import PlotWidget
 from qt_material import apply_stylesheet
 from screeninfo import get_monitors
