b9de58409810476bb22b11042c0c60196cd1da57
vcatlett-nrao <vcatlett@nrao.edu>
2024-08-05 11:33:01 -0400

    fix paths via pathlib (I hope)

--- notebooks/examples/tests/test_notebooks.py
+++ notebooks/examples/tests/test_notebooks.py
@@ -2,15 +2,16 @@ import nbformat
 from nbclient import NotebookClient
 from glob import glob
 import os
+from pathlib import Path
 import pytest
 
 def check_notebook_execution(notebook_file):
     """Execute a given notebook and check for errors"""
 
     # Get the current testing directory
-    cwd = os.getcwd()
+    cwd = Path().cwd()
     # Get the directory of the notebook
-    notebook_dir = os.path.dirname(notebook_file)
+    notebook_dir = Path(notebook_file).parent.resolve()
 
     # Open the notebook
     with open(notebook_file, 'r', encoding='utf-8') as f:
@@ -59,3 +60,6 @@ def test_single_top_level_header():
                         top_level_headers += 1
 
         assert top_level_headers == 1, f"Notebook {notebook_file} has {top_level_headers} top-level headers."
+
+if __name__ == "__main__":
+    test_notebooks_execution()
