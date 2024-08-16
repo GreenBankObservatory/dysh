e629ea183eb3c4d28cb673f6bbbe62290959ee79
Marc Pound <22331890+mpound@users.noreply.github.com>
2024-08-16 15:35:12 -0700

    working history wrapper

--- src/dysh/log.py
+++ src/dysh/log.py
@@ -2,11 +2,16 @@ import inspect
 import logging
 import logging.config
 import os
+import sys
+import time
 from datetime import datetime
 from functools import wraps
 from io import StringIO
 from pathlib import Path
 from typing import Union
+
+import _testcapi
+from astropy.logger import AstropyLogger
 
 try:
     DYSH_LOG_DIR = Path(os.getenv("DYSH_LOG_DIR", "."))
@@ -34,19 +39,23 @@ class StringHandler(logging.StreamHandler):
         super().__init__(stream=string_stream)
 
 
-##class DyshHistoryLogger():
-#  def __init
+dysh_date_format = "%Y-%m-%dT%H:%M:%S%z"
+dysh_history_formatter = logging.Formatter(
+    fmt="%(asctime)s - %(levelname)s - %(modName)s.%(fName)s(%(message)s, %(extra)s)", datefmt=dysh_date_format
+)
+
+
 config = {
     "version": 1,
     "disable_existing_loggers": False,
     "formatters": {
         "extra_verbose": {
             "format": "%(asctime)s - %(pathname)s:%(lineno)s - %(message)s",
-            "datefmt": "%Y-%m-%dT%H:%M:%S%z",
+            "datefmt": dysh_date_format,
         },
         "verbose": {
             "format": "%(asctime)s - %(levelname)s - %(module)s - %(message)s",
-            "datefmt": "%Y-%m-%dT%H:%M:%S%z",
+            "datefmt": dysh_date_format,
         },
         "simple": {
             "format": "%(levelname)s %(message)s",
@@ -56,7 +65,7 @@ config = {
         },
         "data_history": {
             "format": "%(asctime)s - %(levelname)s - %(modName)s.%(fName)s(%(message)s, %(extra)s)",
-            "datefmt": "%Y-%m-%dT%H:%M:%S%z",
+            "datefmt": dysh_date_format,
         },
     },
     "handlers": {
@@ -134,25 +143,85 @@ def config_logging(verbosity: int, level: Union[int, None] = None, path: Union[P
     dhlogger.setLevel(logging.INFO)
 
 
-def log_call(func):
+def log_function_call(func):
     @wraps(func)
-    def wrap(*args, **kwargs):
+    def wrapper(*args, **kwargs):
         # Log the function name and arguments
-        # dhlogger.info(f"calling {args}", extra=kwargs)
         sig = inspect.signature(func)
-        print(sig)
         if "kwargs" in sig.parameters:
             extra = {"modName": func.__module__, "fName": func.__name__, "extra": kwargs}
         else:
-            extra = {"modName": func.__module__, "fName": func.__name__, "extra": ""}
-        dhlogger.info(*args, extra=extra)
+            extra = {"modName": func.__module__, "fName": func.__name__, "extra": {}}
+        logger.info(*args, extra=extra)
         # Call the original function
         result = func(*args, **kwargs)
         # Return the result
         return result
 
-    return wrap
+    return wrapper
+
+
+def format_dysh_log_record(record: logging.LogRecord) -> str:
+    asctime = time.strftime(dysh_date_format, time.localtime(record.created))
+    logmsg = f"{asctime} - {record.levelname} - {record.msg} : {record.modName}."
+    if hasattr(record, "className"):
+        logmsg += f"{record.className}."
+    logmsg += f"{record.fName}("
+    if len(record.args) > 0:
+        logmsg += f"{record.args}"
+    if len(record.kwargs) > 0:
+        for k, v in record.kwargs.items():
+            logmsg += f"{k}={v},"
+    logmsg += ")"
+    return logmsg
+
+
+def log_call_to_history(func):
+    @wraps(func)
+    def wrapper(self, *args, **kwargs):
+        if self is None:  # not a class, but a function
+            logger.warn(
+                f"Function {func.__module__}.{func.__name__} is a function with no _history attribute. Use @log_functiona_call instead."
+            )
+            # could put this try around the whole thing but then it would catch exceptions from the wrapper itself
+            try:
+                result = func(*args, **kwargs)
+            except:  # remove the wrapper from the stack trace
+                tp, exc, tb = sys.exc_info()
+                _testcapi.set_exc_info(tp, exc, tb.tb_next)
+                del tp, exc, tb
+                raise
+        else:  # it's a class instance
+            try:
+                result = func(self, *args, **kwargs)
+            except:  # remove the wrapper from the stack trace
+                tp, exc, tb = sys.exc_info()
+                _testcapi.set_exc_info(tp, exc, tb.tb_next)
+                del tp, exc, tb
+                raise
+            classname = self.__class__.__name__
+            if hasattr(self, "_history"):
+                sig = inspect.signature(func)
+                if "kwargs" in sig.parameters:
+                    extra = {
+                        "modName": func.__module__,
+                        "className": classname,
+                        "fName": func.__name__,
+                        "kwargs": kwargs,
+                    }
+                else:
+                    extra = {"modName": func.__module__, "className": classname, "fName": func.__name__, "extra": {}}
+                with dhlogger.log_to_list() as log_list:
+                    dhlogger.handlers[0].setFormatter(dysh_history_formatter)
+                    dhlogger.info("command", *args, extra=extra)
+                    log_str = [format_dysh_log_record(i) for i in log_list]
+                    self._history.extend(log_str)
+            else:
+                logger.warn(f"Class {func.__module__}.{classname} has no _history attribute to log to")
+        return result
+
+    return wrapper
 
 
 logger = logging.getLogger("dysh")
-dhlogger = logging.getLogger("dysh_history")
+dhlogger = AstropyLogger("dysh_history", level=logging.INFO)
