49a659c64853da0ba526331dff7eb654e32011c8
Peter Teuben <teuben@gmail.com>
2024-09-11 14:05:10 -0400

    placeholder

---
+++ notebooks/developer/test_spectral_alignment.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# fmt: off
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+from specutils import Spectrum1D
+import astropy.units as u
+import numpy as np
+from specutils.manipulation import box_smooth, gaussian_smooth, trapezoid_smooth
+
+
