4bd9e681a84851fb90b133c14e4761b39ed8a0a9
Marc Pound <22331890+mpound@users.noreply.github.com>
2024-09-24 16:43:19 -0700

    skel flag test

--- src/dysh/util/tests/test_selection.py
+++ src/dysh/util/tests/test_selection.py
@@ -7,7 +7,7 @@ from astropy.time import Time
 import dysh
 from dysh import util
 from dysh.fits import gbtfitsload
-from dysh.util.selection import Selection
+from dysh.util.selection import Flag
 
 dysh_root = pathlib.Path(dysh.__file__).parent.resolve()
 
@@ -17,6 +17,8 @@ class TestSelection:
 
     def setup_method(self):
         self.file = util.get_project_testdata() / "TGBT21A_501_11/testselection.fits"
+        self.sdf = gbtfitsload.GBTFITSLoad(self.file)
+        self.gbtidl_flag_file = ""
 
     def test_selection_class(self):
         """
@@ -24,8 +26,7 @@ class TestSelection:
         We check that on each selection, the number of selected rows
         matches the known correct answer.
         """
-        sdf = gbtfitsload.GBTFITSLoad(self.file)
-        s = sdf._selection
+        s = self.sdf.selection
         # polarization 0 and source name
         s.select(object="NGC2415", plnum=0)
         assert len(s._selection_rules[0]) == 5
@@ -55,7 +56,6 @@ class TestSelection:
         # lower limit.
         # RAs range from 53.24 to 247.04 degrees
         # Decs range from -15.64 to 54.57 degrees
-        # s = Selection(sdf)
         s.select_range(ra=(114,))  # default is degrees
         assert len(s.final) == 40
         # check that quantities work
@@ -87,3 +87,9 @@ class TestSelection:
         assert len(s.final) == len(s)
         with pytest.raises(Exception):
             s.select_channel(["10", "a", 103])
+
+    def test_flag_class(self):
+        f = self.sdf.flag
+
+    def test_flag_read(self):
+        f = Flag.read(self.gbtidl_flag_file)
