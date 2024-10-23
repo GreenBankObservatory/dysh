7a83a0e6361821ec3a91f6995079bc358ae156c7
Peter Teuben <teuben@gmail.com>
2024-10-23 10:31:43 -0400

    work in progress

--- notebooks/developer/test_vane.py
+++ notebooks/developer/test_vane.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 """
-This script was developed in spyder during the vane calibration work.
+This script was developed in spyder during the vane calibration work. 
+The following examples are covered here:
 
 From nodding we have two cases:
+getps - review the standard test='getps'
 nod1  fdnum=[10,1]      unbalanced calrows 0 != 6        tp nocal;  TBD
 nod3  fdnum=[0,1]       unbalanced calrows 0 != 6        tp nocal;  TBD
 """
@@ -160,15 +162,139 @@ def calseq(sdf, scan, tcold=54, ifnum=0, plnum=0, fdnum=0):
     """
     return 0.0
 
+# scan = auto calseq scan
+# tcold = effective temperature of cold load (e.g., 50K)
+# ifnum = IFnum of spectral window
+# plnum = pol-number
+# fdnum = beam-number
 
+"""
+;;Output:
+;;Prints Tsys and gain and returns OUTgain
+;;OUTgain= gain = Twarm-Tcold)/(warm-cold) [K/counts]
+;;Tsys=median(gain*sky)
+
+if (n_elements(ifnum) eq 0) then ifnum = 0
+if (n_elements(fdnum) eq 0) then fdnum = 0
+if (n_elements(plnum) eq 0) then plnum = 0
+if (n_elements(tcold) eq 0) then tcold = 54.
+
+# CALPOSITION contains the wcalpos
+
+gettp,scan,plnum=plnum,fdnum=fdnum,ifnum=ifnum,quiet=1,wcalpos='Observing'
+vsky=getdata(0)
+twarm=!g.s[0].twarm
+gettp,scan,plnum=plnum,fdnum=fdnum,ifnum=ifnum,quiet=1,wcalpos='Cold1'
+vcold1=getdata(0)
+gettp,scan,plnum=plnum,fdnum=fdnum,ifnum=ifnum,quiet=1,wcalpos='Cold2'
+vcold2=getdata(0)
+
+
+;;Feed =1 or 2 for the two possible receiver beams
+feed=!g.s[0].feed
+gain=0.0
+if (feed eq 1) then gain=(twarm-tcold)/median(vcold2-vcold1)
+if (feed eq 2) then gain=(twarm-tcold)/median(vcold1-vcold2)
+tsys=median(gain*vsky)
+
+print,'Twarm, Tcold:',twarm,tcold
+print,'IFNUM, FDNUM, PLNUM:',ifnum,fdnum,plnum
+print,'Tsys =',tsys
+print,'Gain [K/counts]=',gain
+OUTgain=gain
+
+"""
+
+def getnod(sdf, scan1, scan2, beam1, beam2):
+    ps1_on = sdf.gettp(scan=scan1, fdnum=beam1, calibrate=True, cal=False).timeaverage()
+    ps1_off = sdf.gettp(scan=scan2, fdnum=beam1, calibrate=True, cal=False).timeaverage()
+    sp1 = (ps1_on - ps1_off)/ps1_off
+
+    ps2_on = sdf.gettp(scan=scan2, fdnum=beam2, calibrate=True, cal=False).timeaverage()
+    ps2_off = sdf.gettp(scan=scan1, fdnum=beam2, calibrate=True, cal=False).timeaverage()
+    sp2 = (ps2_on - ps2_off)/ps2_off
+
+    return (sp1,sp2)
+
+#%%
+
+def calc_etamb(freq, Jupiter=False):
+
+    """
+    For a given input frequency, calculate and return the eta_mb value
+    for the GBT using David's equation from GBT Memo 302. 
+
+    The equation is not included in the memo and provided by private 
+    communication:
+        eta_jupiter = 1.23 * eta_aperture + 0.005 * (nu - 60) - 0.00003*(nu-60)**2
+    where nu is in GHz.
+
+    It comes from a polynominal fit to nu and eta_jupiter using eta_aperture
+    as a function of frequency.
+
+    This correction assumes that our sources are approximately the
+    size of Jupiter (43" diameter), which isn't a bad assumption for
+    extended molecular gas.
+
+    """
+
+    import math
+    from astropy import constants as c
+
+    # surface error for GBT with optimal surface and excellent weather
+    esurf = 0.0235*u.cm # cm; GBT memo 302 says 230micron = 0.0230
+
+    # make the input frequency into a quantity if it isn't already
+    if not isinstance(freq,u.Quantity):
+        if freq > 1.0e9: # freq likely in Hz
+            freq = freq * u.Hz
+        elif freq < 200.0: # freq likely in GHz
+            freq = freq * u.GHz
+        else:
+            print("check units on input frequency. Should be either in GHz or Hz")
+    # convert frequency to GHz to use in fit
+    freq = freq.to(u.GHz)
+
+    # calculate equivalent wavelength
+    wave = freq.to(u.cm, equivalencies=u.spectral()) 
+
+    # aperture efficiency for GBT using Ruze equation.
+    # 0.71 is the aperture efficiency of the GBT at lower frequency.
+    eta_a = 0.71 * math.exp(- ( 4 * math.pi * esurf / wave)**2)
+
+    if Jupiter:
+        # calculate eta_mb for a jupiter sized source via 
+        # polynominal fit from GBT Memo 302.  The equation is not
+        # included in the memo and provided by private communication:
+        #        eta_jupiter = 1.23 * eta_aperture + 0.005 * (nu - 60) -
+        #        0.00003*(nu-60)**2 
+        # where nu is in GHz. This correction assumes that our sources 
+        # are approximately the size of Jupiter (43" diameter)
+        eta_mb = 1.23 * eta_a + 0.005*(freq.value-60) - 0.00003 * (freq.value - 60)**2
+    # else calculate "small source" eta_mb.
+    elif freq > 110.0*u.GHz:
+        # corrected for higher freq (D.Frayer) 
+        eta_mb = 1.24 * (freq.value/113)**0.8 * math.exp ( -(1.3*freq.value/113)**2 )
+    elif freq > 100.0*u.GHz:
+        # GBT memo 302 finds that at high frequencies the eta_mb/eta_b ratio is more like 1.45 due to a slightly larger beam size factor (1.28 instead of 1.2).
+        eta_mb = 1.45 * eta_a
+    else: 
+        # GBT memo 302 finds that for frequencies of 86-90GHz you can use the expected theoretical ratio of 1.274
+        eta_mb = 1.274 * eta_a
+
+    print("ETA A,MB: ",eta_a, eta_mb, "at freq ", freq)
         
+    return (eta_a, eta_mb)
+
+
+
 #%%  classic tp/ps
 
 f1 = dysh_data(test="getps")        # OnOff, small one (1 IF, 1 PL, 1 INT)
 #f1 = dysh_data(example="getps")    # OnOff, long one (4 IFs, 2 PLs, 151 INTs)
 #f1 = dysh_data(test="AGBT18B_354_03/AGBT18B_354_03.raw.vegas") # OffOn
-
-print("Using",f1.parts[-1])     # isn't there a better name for this?
+fn1 = f1.parts[-1]
+print(f"Using {fn1}")
 
 
 sdf1 = GBTFITSLoad(f1)
@@ -177,17 +303,19 @@ sdf1.summary(verbose=True)
 sdf1._index[k]
 
 sp1 = sdf1.getps().timeaverage()
-sp1.plot()
+sp1.plot(title='getps: %s' % fn1)
 sp1.stats(qac=True)
 # 0.21853874407385052 0.6796743343920357 -3.7057502269744873 4.343878746032715
 
 tp1a = sdf1.gettp(scan=152).timeaverage()
 tp1b = sdf1.gettp(scan=153).timeaverage()
 
-tp1a.plot()
-tp1b.plot()
+tp1a.plot(title='gettp scan=152')
+tp1b.plot(title='gettp scan=153')
 
 tp1a.stats(qac=True)  # '490015338.1141468 152969155.2487644 3311173.5 805602304.0'
+
+
 
 #%% NOD EXAMPLE-1   tp_nocal    NOD_BEAMS  10,1   (FEED 11,2)
 
@@ -217,16 +345,21 @@ vcal(vane1, 281, 282)
 v1 = vane1.getspec(0).flux.value
 s1 = vane1.getspec(4).flux.value
 t1 = s1/(v1-s1)                          # 0.53 +/0 0.02
-np.nanmean(t1[100:900])
-np.nanstd(t1[100:900])
+np.nanmean(t1[100:900])  # 0.52613854
+np.nanstd(t1[100:900])   # 0.016047847
 
 v1 = vane1.gettp(scan=281, fdnum=8, calibrate=True, cal=False).timeaverage()
 # ok
 s1 = vane1.gettp(scan=282, fdnum=8, calibrate=True, cal=False).timeaverage()
 # ok
 
-sp = sdf1.gettp(scan=281, fdnum=8, calibrate=True, cal=False).timeaverage().plot()
-# ok
+t1 = s1/(v1-s1) 
+t1 = t1[100:900]
+# UnitTypeError: Longitude instances require units equivalent to 'rad', so cannot set it to ''
+
+
+v1.plot(title='VANE')
+s1.plot(title='SKY')
 
 print(vanecal(vane1, 281, 282, feeds=range(16)))
 # [1.21549487 1.07764171 4.13970304 1.15904234 0.9971537  1.122969
@@ -234,7 +367,15 @@ print(vanecal(vane1, 281, 282, feeds=range(16)))
 # 0.99750374 1.02722791 1.00445433 1.00216715]
 
 
-#%%   EDGE data  (1363MB)
+sp1,sp2 = getnod(sdf1, 289, 290, 1, 10)
+sp1.plot()
+sp2.plot()
+
+sp3 = 0.5*(sp1+sp2)
+sp3.plot()
+
+
+#%%   EDGE1 data  (1363MB)
 
 _help = """
      SCAN    OBJECT VELOCITY       PROC  PROCSEQN    RESTFREQ     DOPFREQ # IF # POL # INT # FEED     AZIMUTH   ELEVATIO
@@ -281,6 +422,74 @@ edge1.gettp(scan=17, fdnum=8, calibrate=True, cal=False).timeaverage().plot()
 # not working yet, since it wants ON/OFF, needs the GETTP hack
 sp2 = sdf2.getnod(scan=19)
 # IndexError: index 0 is out of bounds for axis 0 with size 0
+
+
+# if getnod() is not converted, we can use the old trick of doing two gettp()
+scan1 = 19
+scan2 = 20
+beam1 = 4
+beam2 = 7
+sp1,sp2 = getnod(edge1, scan1, scan2, beam1, beam2)
+
+sp3 = 0.5*(sp1+sp2)
+sp3.plot(xaxis_unit="km/s")
+# center is at 0 km/s, that seems wrong, should be VLSR 4500 km/s at 113.57
+
+
+#%% EDGE2 nod example on NGC5908
+sdf1=GBTFITSLoad(dysh_data('AGBT21B_024_14/AGBT21B_024_14.raw.vegas'))
+
+mkdir("edge2")
+sdf1.write("edge2/file.fits", scan=[327,328,329,330,331,332,333,334], overwrite=True)                # 4576 rows
+#  CPU times: user 23.5 s, sys: 3.02 s, total: 26.5 s   Wall time: 25.2 s
+
+edge2 = GBTFITSLoad("edge2")
+edge2.summary()
+edge2._index[ks] # 5248 rows
+
+_s = """
+   SCAN   OBJECT VELOCITY   PROC  PROCSEQN    RESTFREQ     DOPFREQ # IF # POL # INT # FEED     AZIMUTH   ELEVATIO
+0   327     VANE      0.0  Track         1  112.365952  112.365952    1     1    21     16  314.014676  69.054141
+1   328      SKY      0.0  Track         1  112.365952  112.365952    1     1    21     16  314.015393  69.054142
+2   329     VANE      0.0  Track         1   114.03043   114.03043    1     1    21     16  333.855801  70.214517
+3   330      SKY      0.0  Track         1   114.03043   114.03043    1     1    21     16  333.856727  70.214422
+4   331  NGC5908      0.0    Nod         1   114.03043   114.03043    1     1    61     16  333.089151  70.104129
+5   332  NGC5908      0.0    Nod         2   114.03043   114.03043    1     1    61     16  332.870869   70.02778
+6   333  NGC5908      0.0    Nod         1   114.03043   114.03043    1     1    61     16  332.654981  69.984783
+7   334  NGC5908      0.0    Nod         2   114.03043   114.03043    1     1    61     16   332.44083  69.907597
+"""
+tsys1 = vanecal(edge2, 327, 328, feeds=range(16))
+print(tsys1)
+# @112.3 GHz
+# [1.20676998 1.12933116 1.09430538 1.18707773 1.08870158 1.38427164
+#  1.08151629 1.06933859 1.07527814 1.06119602 1.07773119 1.09914186
+#  1.05032223 1.09743774 1.06316352 1.04670035]
+
+
+tsys2 = vanecal(edge2, 329, 330, feeds=range(16))
+print(tsys2)
+# @114.0 GHz
+# [1.40294513 1.2976351  1.28455571 1.16614836 1.21608555 1.37295378
+# 1.27542001 1.22878323 1.23203965 1.23371031 1.25607937 1.23815973
+# 1.2139721  1.29401993 1.17909461 1.2009243 ]
+
+# difference:   0.14 +/- 0.06
+
+sp1,sp2 = getnod(edge2, 331, 332, 4, 7)
+
+sp3 = 0.5*(sp1+sp2)
+sp3.plot()
+sp3.stats(roll=1, qac=True)   # 4.923401847859579e-05 0.0016115564993204495 -0.0007704968985869036 0.051103618263309156'
+
+
+sp1,sp2 = getnod(edge2, 333, 334, 4, 7)
+
+sp3 = 0.5*(sp1+sp2)
+sp3.plot()
+sp3.stats(roll=1,qac=True)   # 3.4933664104756355e-06 0.00023323680056886874 -0.0008156976082961529 0.0034146710055591675'
+
+# no obvious signal
+
 
 #%% NOD EXAMPLE-3 tp_nocal   NOD_BEAMS 0,1
 
@@ -364,7 +573,7 @@ sp4b = sdf4.gettp(scan=1, plnum=1)
 sp4a.timeaverage().plot(ymin=-1e8, ymax=1e9, title="TSCI_RYAN_16 scan=1 plnum=0", xaxis_unit="chan")
 sp4b.timeaverage().plot(ymin=-1e8, ymax=1e9, title="TSCI_RYAN_16 scan=1 plnum=1", xaxis_unit="chan")
      
-sp = sdf4.gettp(scan=2, plnum=0, calibrate=True, cal=False).timeaverage().plot(ymin=-1e8, ymax=1e9)
+sdf4.gettp(scan=2, plnum=0, calibrate=True, cal=False).timeaverage().plot(ymin=-1e8, ymax=1e9)
 # ok
 
 #%%  VANE5   argus TGBT22A_605_05 
@@ -391,8 +600,18 @@ sdf5 = GBTFITSLoad(f5)
 sdf5.summary()
 
 tsys_10 = vanecal(sdf5, 10, 11, feeds=range(16))
+#      1.45318402, 1.25949678, 1.27948539, 1.41625876, 1.26606387,
+#      1.42277304, 1.2289571 , 1.29762214, 1.27845341, 1.24670253,
+#      1.24174926, 1.32368279, 1.2663306 , 1.2432676 , 1.29513699,
+#      1.23222654])
+ 
 tsys_34 = vanecal(sdf5, 34, 35, feeds=range(16))
+#      [1.46858929, 1.27776463, 1.29033069, 1.43358742, 1.28317839,
+#       1.42513274, 1.24034782, 1.31302693, 1.29617406, 1.26008328,
+#       1.25831389, 1.34137986, 1.06626227, 1.25749316, 1.32069702,
+#       1.252414  ])
 
+# mean and std of diff:    -0.0020865396509081036  0.05241421768622772
 
 
 #%%  VANE6   W-band rxco-W/data/TSCAL_220105_W.raw.vegas
@@ -414,14 +633,15 @@ _help = """
 """
 
 f6 = dysh_data(example="rxco-W/data/TSCAL_220105_W.raw.vegas")
-sdf6 = GBTFITSLoad(f1)
+sdf6 = GBTFITSLoad(f6)
 sdf6.summary()
 sdf6._index[kw]    # 1728
 
 mkdir("vane6")
-sdf6.write("vane6/file.fits", plnum=0, intnum=0, overwrite=True)
+sdf6.write("vane6/file.fits", plnum=0, scan=32, overwrite=True)
 
 vane6 = GBTFITSLoad("vane6")
-
+vane6.summary()
+vane6._index[kw]
 
 
