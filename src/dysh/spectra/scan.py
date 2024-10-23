fix test_slice test by adding observer location to spectral average

src/dysh/spectra/scan.py
@@ -437,10 +437,7 @@ class ScanBlock(UserList, HistoricalBase, SpectralAverageMixin):
                 # timeavg = np.ma.empty((np.shape(self._timeaveraged)[0],np.shape(self._timeaveraged[0])[0]))
                 # Weight the average of the timeaverages by the weights.
                 avgdata = average(timeavg, axis=0, weights=w)
-                print(f"{type(timeavg)=}, {type(avgdata)=}")
                 avgspec = np.ma.mean(self._timeaveraged)
-                print(f"{type(avgspec)=}")
-                print(f"{avgspec.mask=}")
                 avgspec.meta = self._timeaveraged[0].meta
                 avgspec.meta["TSYS"] = np.average(a=[k.meta["TSYS"] for k in self._timeaveraged], axis=0, weights=w)
                 avgspec.meta["EXPOSURE"] = np.sum([k.meta["EXPOSURE"] for k in self._timeaveraged])
@@ -452,7 +449,6 @@ class ScanBlock(UserList, HistoricalBase, SpectralAverageMixin):
                     observer_location=Observatory["GBT"],
                 )
 
-            print(f"{self._timeaveraged[0].mask=}")
             s = average_spectra(self._timeaveraged)
             s.merge_commentary(self)
         elif mode == "new":
@@ -1183,6 +1179,7 @@ class PSScan(ScanBase):
         self._timeaveraged.meta["EXPOSURE"] = np.sum(self._exposure[non_blanks])
         self._timeaveraged.meta["TSYS"] = self._timeaveraged.meta["WTTSYS"]
         self._timeaveraged._history = self._history
+        self._timeaveraged._observer_location = self._observer_location
         print(
             "PS TA OBS ",
             self._timeaveraged._observer_location,

src/dysh/spectra/spectrum.py
@@ -1561,7 +1561,7 @@ def average_spectra(spectra, equal_weights=False, align=False):
     tsyss = np.empty(nspec, dtype=float)
     xcoos = np.empty(nspec, dtype=float)
     ycoos = np.empty(nspec, dtype=float)
-
+    obs_location = spectra[0]._observer_location
     units = spectra[0].flux.unit
 
     for i, s in enumerate(spectra):
@@ -1574,7 +1574,7 @@ def average_spectra(spectra, equal_weights=False, align=False):
         if align:
             if i > 0:
                 s = s.align_to(spectra[0])
-        logger.debug(f"OBS LOCATION  {s._observer_location}")
+
         data_array[i] = s.data
         data_array[i].mask = s.mask
         if not equal_weights:
@@ -1599,6 +1599,6 @@ def average_spectra(spectra, equal_weights=False, align=False):
     new_meta["CRVAL2"] = xcoo
     new_meta["CRVAL3"] = ycoo
 
-    averaged = Spectrum.make_spectrum(Masked(data * units, data.mask), meta=new_meta)
+    averaged = Spectrum.make_spectrum(Masked(data * units, data.mask), meta=new_meta, observer_location=obs_location)
 
     return averaged