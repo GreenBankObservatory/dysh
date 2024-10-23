fix failing test. rewrite interface of average_spectra so weights keyword is consistent with other methods

src/dysh/spectra/spectrum.py
@@ -1529,7 +1529,7 @@ with registry.delay_doc_updates(Spectrum):
     # registry.register_writer("mrt", Spectrum, spectrum_reader_mrt)
 
 
-def average_spectra(spectra, equal_weights=False, align=False):
+def average_spectra(spectra, weights="tsys", align=False):
     """
     Average `spectra`. The resulting `average` will have an exposure equal to the sum of the exposures,
     and coordinates and system temperature equal to the weighted average of the coordinates and system temperatures.
@@ -1539,9 +1539,12 @@ def average_spectra(spectra, equal_weights=False, align=False):
     spectra : list of `Spectrum`
         Spectra to be averaged. They must have the same number of channels.
         No checks are done to ensure they are aligned.
-    equal_weights : bool
-        If `False` use the inverse of the variance, as computed from the radiometer equation, as weights.
-        If `True` all spectra have the same weight.
+    weights: str
+        'tsys' or None.  If 'tsys' the weight will be calculated as:
+
+         :math:`w = t_{exp} \times \delta\nu/T_{sys}^2`
+
+        Default: 'tsys'
     align : bool
         If `True` align the `spectra` to the first element.
         This uses `Spectrum.align_to`.
@@ -1556,7 +1559,7 @@ def average_spectra(spectra, equal_weights=False, align=False):
     nchan = len(spectra[0].data)
     shape = (nspec, nchan)
     data_array = np.ma.empty(shape, dtype=float)
-    weights = np.empty(shape, dtype=float)
+    wts = np.empty(shape, dtype=float)
     exposures = np.empty(nspec, dtype=float)
     tsyss = np.empty(nspec, dtype=float)
     xcoos = np.empty(nspec, dtype=float)
@@ -1566,7 +1569,7 @@ def average_spectra(spectra, equal_weights=False, align=False):
 
     for i, s in enumerate(spectra):
         if not isinstance(s, Spectrum):
-            raise ValueError(f"Element {i} of `spectra` is not a `Spectrum`.")
+            raise ValueError(f"Element {i} of `spectra` is not a `Spectrum`. {type(s)}")
         if units != s.flux.unit:
             raise ValueError(
                 f"Element {i} of `spectra` has units {s.flux.unit}, but the first element has units {units}."
@@ -1577,20 +1580,19 @@ def average_spectra(spectra, equal_weights=False, align=False):
 
         data_array[i] = s.data
         data_array[i].mask = s.mask
-        if not equal_weights:
-            weights[i] = core.tsys_weight(s.meta["EXPOSURE"], s.meta["CDELT1"], s.meta["TSYS"])
+        if weights == "tsys":
+            wts[i] = core.tsys_weight(s.meta["EXPOSURE"], s.meta["CDELT1"], s.meta["TSYS"])
         else:
-            weights[i] = 1.0
+            wts[i] = 1.0
         exposures[i] = s.meta["EXPOSURE"]
         tsyss[i] = s.meta["TSYS"]
         xcoos[i] = s.meta["CRVAL2"]
         ycoos[i] = s.meta["CRVAL3"]
-
     data_array = np.ma.MaskedArray(data_array, mask=np.isnan(data_array) | data_array.mask)
-    data = np.ma.average(data_array, axis=0, weights=weights)
-    tsys = np.ma.average(tsyss, axis=0, weights=weights[:, 0])
-    xcoo = np.ma.average(xcoos, axis=0, weights=weights[:, 0])
-    ycoo = np.ma.average(ycoos, axis=0, weights=weights[:, 0])
+    data = np.ma.average(data_array, axis=0, weights=wts)
+    tsys = np.ma.average(tsyss, axis=0, weights=wts[:, 0])
+    xcoo = np.ma.average(xcoos, axis=0, weights=wts[:, 0])
+    ycoo = np.ma.average(ycoos, axis=0, weights=wts[:, 0])
     exposure = exposures.sum(axis=0)
 
     new_meta = deepcopy(spectra[0].meta)