d7f374b8f81f42e8c250b209d3be4e9550b89685
Marc Pound <22331890+mpound@users.noreply.github.com>
2024-12-13 17:28:54 -0800

    debugging

--- src/dysh/util/gaincorrection.py
+++ src/dysh/util/gaincorrection.py
@@ -218,6 +218,7 @@ class GBTGainCorrection(BaseGainCorrection):
         a0 = self._gct[i]["A0"]
         a1 = self._gct[i]["A1"]
         a2 = self._gct[i]["A2"]
+        print(f"Using {a0=} {a1=} {a2=} for {date}")
         ang_deg = angle.to(u.degree)
         if not zd:
             z = 90.0 - ang_deg.value
