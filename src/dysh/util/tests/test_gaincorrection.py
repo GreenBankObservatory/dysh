d462ee700a2df9a1d6148406fcf31ed7e078cffd
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-01-04 11:29:11 -0800

    add tests of GBT memo 301 figure 1 curves

--- src/dysh/util/tests/test_gaincorrection.py
+++ src/dysh/util/tests/test_gaincorrection.py
@@ -37,12 +37,74 @@ class TestGainCorrection:
             assert se == answer[i]
 
     def test_gaincorrection_factor(self):
-        angles = Angle([5.0, 25.0, 40.0, 45.0, 50, 65.0, 85.0], unit=u.degree)
-        answer = np.array([])
+        # first test use symmetric angles around 45
+        angles = Angle([0.0, 25.0, 40.0, 45.0, 50, 65.0, 90.0], unit=u.degree)
+        # answer = np.array([])
         for i in range(len(self.dates)):
             gc1 = self.gbtgc.gain_correction(angles, self.dates[i], zd=True)
             gc2 = self.gbtgc.gain_correction(angles, self.dates[i], zd=False)
             assert all(gc1 == gc2[::-1])
+        # now try to reproduce portion of figure 1 in GBT memo 301
+        angles = Angle(np.arange(10, 90, 5), unit=u.degree)
+        scale = 0.983  # 2009b gmax from Dave Frayer email 12/18/2024
+        gc = scale * self.gbtgc.gain_correction(angles, self.dates[4], zd=False)
+        # measured using graphreader.com on a screenshot of Figure 1 from GBT memo 301 for "2009b" curve
+        answer = np.array(
+            [
+                0.777,
+                0.819,
+                0.858,
+                0.893,
+                0.922,
+                0.944,
+                0.961,
+                0.974,
+                0.981,
+                0.985,
+                0.981,
+                0.971,
+                0.957,
+                0.939,
+                0.912,
+                0.882,
+            ]
+        )
+        assert np.abs(np.mean(gc - answer)) < 0.001
+        # measured using graphreader.com on a screenshot of Figure 1 from GBT memo 301 for "2009a" curve
+        gc *= 0.728  # 2009a gmax from Dave Frayer email 12/18/2024.  Yes it is a double scale 0.983*0.728
+        answer = np.array(
+            [
+                0.565,
+                0.597,
+                0.625,
+                0.65,
+                0.67,
+                0.688,
+                0.701,
+                0.71,
+                0.715,
+                0.716,
+                0.714,
+                0.706,
+                0.696,
+                0.683,
+                0.665,
+                0.642,
+            ]
+        )
+        assert np.abs(np.mean(gc - answer)) < 0.001
+        # 2014 curve.
+        gc = self.gbtgc.gain_correction(angles, self.dates[-1], zd=False)
+        # measured using graphreader.com on a screenshot of Figure 1 from GBT memo 301 for "2014" curve
+        # harder to do because of graph crowding.
+        answer = np.array([0.987, 0.991, 0.994, 0.997, 0.999, 1, 1, 1, 1, 1, 0.998, 0.995, 0.991, 0.987, 0.983, 0.978])
+        assert np.abs(np.mean(gc - answer)) < 0.001
+        #
+        # 2003 curve was not generated using G(ZD) so we do not test it here.
+        # according to Frayer it is (IDL code):
+        # ;;No Gravity corrections Condon 2003 PTCS/PN/31.1
+        # ;;max eta_a=43% (390um rms) scaled to max of 60% fac=0.43/0.60=0.717
+        # nograv=0.717*exp(-1.0*((xel-52.0)/54.)^2.)
 
     def test_aperture_efficiency(self):
         """This attempts to reproduce Table 2 in GBT Memo 301"""
