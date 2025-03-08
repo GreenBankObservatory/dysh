c17b520c520022f820233e95403833ad4917ca0c
Peter Teuben <teuben@gmail.com>
2025-03-07 19:08:00 -0500

    added CALSEQ test

--- src/dysh/spectra/tests/test_vane.py
+++ src/dysh/spectra/tests/test_vane.py
@@ -12,13 +12,14 @@ from dysh.fits.core import mean_data
 from dysh.fits.core import getbeam
 from dysh.fits.core import calseq
 from dysh.fits.core import vanecal
-from dysh.fits.core import plot_vegas      # different location ?
-from dysh.fits.core import getnod          # should be done by official getnod
+from dysh.fits.core import plot_vegas
+from dysh.fits.core import getnod
 
 class TestVaneScan:
-    def test_vane1(self, data_dir):
+    def test_vane1(self):
         """
-        test1
+        test the vane/sky calibration of an ARGUS project
+        using the VANECAL procedure
         """
         rtol = 1e-4      # do tests on this relative tolerance
         
@@ -38,14 +39,39 @@ class TestVaneScan:
         sp1,sp2 = getnod(sdf2, [331, 332], beam2, tsys=tsys2)
         sp3,sp4 = getnod(sdf2, [333, 334], beam2, tsys=tsys2)
         sp5 = sp1.average([sp2,sp3,sp4])
+        
         tint = sp5.meta["EXPOSURE"]
         assert np.isclose(tint, 3.942, rtol=rtol)    # 3.941774845123291
         
         tsys = sp5.meta["TSYS"] 
         assert np.isclose(tsys, 210.54, rtol=rtol)  # 210.54325052318202
 
-        #s4 = [float(s) for s in sp5.stats(qac=True).split()]
         rms = sp5.stats()['rms'].value           #  0.2357084722315336
-        print(rms)
         assert np.isclose(rms, 0.47509, rtol=rtol)
+
+    def test_calseq2(self):
+        """
+        test the vane calibration of a W-band observation
+        using the CALSEQ procedure
+        """
+        rtol = 1e-4      # do tests on this relative tolerance
+        sdf_file = dysh_data(test="AGBT15B_244_07/AGBT15B_244_07_test")
+        sdf3 = gbtfitsload.GBTFITSLoad(sdf_file)
+
+        beam3 = getbeam(sdf3)
+        assert len(beam3) == 2
+        assert beam3[0] == 0 and beam3[1] == 1
+
+        tsys3,g = calseq(sdf3, 130, ifnum=1, plnum=0)   # 103.0501604820638
+        assert np.isclose(tsys3, 103.05, rtol=rtol)
+
+        sp1,sp2 = getnod(sdf3, [131,132], beam3, ifnum=1, plnum=0, tsys=tsys3)
+        sp3 = sp1.average(sp2)
+        
+        tint = sp3.meta["EXPOSURE"]   # 11.939691305160522
+        assert np.isclose(tint, 11.94, rtol=rtol)
+
+        rms = sp3.stats()['rms'].value           # 0.2138536948704338
+        assert np.isclose(rms, 0.21385, rtol=rtol)
+        
         
