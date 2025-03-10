f61f0182f31bb9a54e355b616390810dea01b55e
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-03-10 15:41:59 -0700

    remove GainCorrection from auto import

--- src/dysh/util/__init__.py
+++ src/dysh/util/__init__.py
@@ -1,7 +1,7 @@
 """This subpackage contains general utility classes and functions"""
 
 __all__ = ["core", "selection", "download", "gaincorrection", "weatherforecast"]
+from dysh.util.selection import Flag  # noqa:F401
+from dysh.util.selection import Selection  # noqa:F401
+
 from .core import *
-from .gaincorrection import GBTGainCorrection  # noqa:F401
-from .selection import Flag  # noqa:F401
-from .selection import Selection  # noqa:F401
