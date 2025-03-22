70d1b3b07a8f7cdb11a7596d781ed38c8e57149b
astrofle <psalas@nrao.edu>
2025-03-21 20:44:54 -0400

    One more try

--- src/dysh/config/tests/test_config_core.py
+++ src/dysh/config/tests/test_config_core.py
@@ -1,5 +1,7 @@
 """Tests for dysh.config.core"""
 
+from astropy.config import set_temp_config
+
 import dysh.config as dc
 
 
@@ -9,11 +11,13 @@ class TestConfig:
         path = dc.get_config_dir_path(rootname="dysh-test")
         assert path.exists()
 
-    def test_create_config_file(self):
-        assert dc.create_config_file("dysh", rootname="dysh-test")
-        path = dc.get_config_dir_path(rootname="dysh-test")
-        print(path)
-        assert (path / "dysh.cfg").is_file()
+    def test_create_config_file(self, tmp_path):
+        # configuration._override_config_file =
+        with set_temp_config(tmp_path):
+            assert dc.create_config_file("dysh", rootname="dysh-test")
+            path = dc.get_config_dir_path(rootname="dysh-test")
+        print(tmp_path)
+        assert (tmp_path / "dysh-test/dysh.cfg").is_file()
         # Clean up.
-        (path / "dysh.cfg").unlink()
-        path.rmdir()
+        # (path / "dysh.cfg").unlink()
+        # path.rmdir()
