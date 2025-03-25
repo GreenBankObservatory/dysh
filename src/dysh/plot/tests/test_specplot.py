92c9a85bbdf14dbd20cd360183893d1b19127a9b
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-03-25 15:31:01 -0700

    A6 rather than A, as A is not in the repo

--- src/dysh/plot/tests/test_specplot.py
+++ src/dysh/plot/tests/test_specplot.py
@@ -16,7 +16,7 @@ class TestSpecplot:
         Test that plots are saved.
         """
         p = get_project_testdata()
-        sdf = GBTFITSLoad(p / "AGBT20B_014_03.raw.vegas/AGBT20B_014_03.raw.vegas.A.fits")
+        sdf = GBTFITSLoad(p / "AGBT20B_014_03.raw.vegas/AGBT20B_014_03.raw.vegas.A6.fits")
         tp = sdf.gettp(scan=6, plnum=0, ifnum=0, fdnum=0).timeaverage()
         tp.plot()
         of = tmp_path / "test_savefig.png"
