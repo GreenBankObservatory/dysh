46c49408ea138266c3804d9c3698d7dcd65e3be2
Thomas Chamberlin <tchamber@nrao.edu>
2025-03-28 09:53:26 -0400

    Run ruff format

--- src/dysh/spectra/scan.py
+++ src/dysh/spectra/scan.py
@@ -428,9 +428,9 @@ class ScanBase(HistoricalBase, SpectralAverageMixin):
         self._meta = df.to_dict("records")  # returns dict(s) with key = row number.
         for i in range(len(self._meta)):
             if "CUNIT1" not in self._meta[i]:
-                self._meta[i][
-                    "CUNIT1"
-                ] = "Hz"  # @todo this is in gbtfits.hdu[0].header['TUNIT11'] but is it always TUNIT11?
+                self._meta[i]["CUNIT1"] = (
+                    "Hz"  # @todo this is in gbtfits.hdu[0].header['TUNIT11'] but is it always TUNIT11?
+                )
             self._meta[i]["CUNIT2"] = "deg"  # is this always true?
             self._meta[i]["CUNIT3"] = "deg"  # is this always true?
             restfrq = self._meta[i]["RESTFREQ"]
@@ -1938,8 +1938,8 @@ class FSScan(ScanBase):
         fold=True or fold=False is required
         """
         if self._debug:
-            logger.debug(f'FOLD={kwargs["fold"]}')
-            logger.debug(f'METHOD={kwargs["shift_method"]}')
+            logger.debug(f"FOLD={kwargs['fold']}")
+            logger.debug(f"METHOD={kwargs['shift_method']}")
 
         # some helper functions, courtesy proto_getfs.py
         def channel_to_frequency(crval1, crpix1, cdelt1, vframe, nchan, nint, ndim=1):
