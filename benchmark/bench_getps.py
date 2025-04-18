51d98f03b4c5b3f369a8ae73e0b8a5fcae9fd9c6
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-04-18 09:45:46 -0400

    dobench, statslines options

--- benchmark/bench_getps.py
+++ benchmark/bench_getps.py
@@ -24,8 +24,6 @@ def mkdir(name, clean=True):
             print(f"Removing {fn} from {name}")
             os.unlink(os.path.join(name,fn))
 
-
-
 progname  = "bench_getps"
 benchname = "positionswitch"
 data_dir  = "/lma1/teuben/GBT/dysh_data/sdfits/"      # should migrate to use dysh_data or $DYSH_DATA
@@ -33,15 +31,16 @@ data_dir  = "/lma1/teuben/GBT/dysh_data/sdfits/"      # should migrate to use dy
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog=progname)
     # parser.add_argument("--file",        "-f", action="store",       help="input filename", required=True)
+    parser.add_argument("--dobench",     "-d", action="store_true",  help="do the benchmark test")
+    parser.add_argument("--timeaverage", "-t", action="store_true",  help="time average the Scanblocks to make a Spectrum")
+    parser.add_argument("--nocalibrate", "-n", action="store_true",  help="DON'T calibrate the data", default=False)
+    parser.add_argument("--loop",        "-l", action="store",       help="number of times to loop", default=4)
+    parser.add_argument("--skipflags",   "-s", action="store_true",  help="skip reading flags")
     parser.add_argument("--out",         "-o", action="store",       help="output filename (astropy Table)", required=False)
     parser.add_argument("--append",      "-a", action="store_true",  help="append to previous output file (astropy Table)", required=False)
     parser.add_argument("--overwrite",   "-w", action="store_true",  help="overwrite a previous output file (astropy Table)", required=False)
     parser.add_argument("--profile",     "-p", action="store_true",  help="run the profiler")
-    parser.add_argument("--dosomething", "-d", action="store_true",  help="do an optional action")
-    parser.add_argument("--timeaverage", "-t", action="store_true",  help="time average the Scanblocks to make a Spectrum")
-    parser.add_argument("--nocalibrate", "-n", action="store_true", help="DON'T calibrate the data", default=False)
-    parser.add_argument("--loop",        "-l", action="store",       help="number of times to loop", default=4)
-    parser.add_argument("--skipflags",   "-s", action="store_true",  help="skip reading flags")
+    parser.add_argument("--statslines",  "-e", action="store",      help="number of profiler statistics lines to print", default=25)
     parser.add_argument("--quit",        "-q", action="store_true",  help="quit early")    
     #parser.add_argument("--index", "-i", action="store_true", help="create dysh index table (pandas)")
     args = parser.parse_args()
@@ -76,8 +75,8 @@ if __name__ == "__main__":
     time_data.append(time.perf_counter_ns())
     time_stats.append(["load", time_data[-1]])
     calibrate = not args.nocalibrate
-    if args.dosomething:
-        scans = [51,53,55,57]
+    if args.dobench:
+        #scans = [51,53,55,57]
         scans = [51]
         for i in range(1,int(args.loop)+1):
             sb = sdf1.getps(scan=scans, fdnum=0, ifnum=0, plnum=0, calibrate=calibrate)
@@ -97,7 +96,8 @@ if __name__ == "__main__":
     if args.profile:
         pr.disable()
         ps = pstats.Stats(pr).sort_stats(SortKey.CUMULATIVE)
-        ps.print_stats(25)
+        #ps = pstats.Stats(pr).sort_stats(SortKey.CUMULATIVE, SortKey.TIME)
+        ps.print_stats(int(args.statslines))
 
     for i in range(len(time_stats)):
         table.add_row(time_stats[i])
