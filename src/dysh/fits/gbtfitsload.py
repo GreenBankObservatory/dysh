c8be872c2346ce210bdbae5b2f9c0c51d41032fe
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-04-18 10:50:46 -0400

    add nfiles kwarg to limit number of FITS files load. For use in Performance testing

--- src/dysh/fits/gbtfitsload.py
+++ src/dysh/fits/gbtfitsload.py
@@ -68,7 +68,7 @@ class GBTFITSLoad(SDFITSLoad, HistoricalBase):
 
     @log_call_to_history
     def __init__(self, fileobj, source=None, hdu=None, skipflags=False, **kwargs):
-        kwargs_opts = {"index": True, "verbose": False, "fix_ka": True}  # only set to False for performance testing.
+        kwargs_opts = {"index": True, "verbose": False, "fix_ka": True}  # only set index to False for performance testing.
         HistoricalBase.__init__(self)
         kwargs_opts.update(kwargs)
         path = Path(fileobj)
@@ -86,11 +86,14 @@ class GBTFITSLoad(SDFITSLoad, HistoricalBase):
             logger.debug(f"Treating given path {path} as a directory")
             # Find all the FITS files in the directory and sort alphabetically
             # because e.g., VEGAS does A,B,C,D,E
+            nf = 0  # performance testing
             for f in sorted(path.glob("*.fits")):
                 logger.debug(f"Selecting {f} to load")
                 if kwargs.get("verbose", None):
                     print(f"Loading {f}")
-                self._sdf.append(SDFITSLoad(f, source, hdu, **kwargs_opts))
+                if nf < kwargs.get('nfiles',99999): #performance testing limit number of files loaded
+                    self._sdf.append(SDFITSLoad(f, source, hdu, **kwargs_opts))
+                    nf+=1
             if len(self._sdf) == 0:  # fixes issue 381
                 raise Exception(f"No FITS files found in {fileobj}.")
             if not hasattr(self, "_filename"):
