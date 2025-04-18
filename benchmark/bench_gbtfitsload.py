30fab797b4962115f58d896ed6660a33d7d79259
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-04-18 11:58:22 -0400

    add filestats method

--- benchmark/bench_gbtfitsload.py
+++ benchmark/bench_gbtfitsload.py
@@ -6,10 +6,12 @@ import os
 import pstats
 import sys
 import time
+from pathlib import Path
 from pstats import SortKey
 
 import numpy as np
 from astropy.table import Table,vstack
+import astropy.units as u
 from dysh.fits.gbtfitsload import GBTFITSLoad
 from dysh.util.files import dysh_data
 
@@ -24,6 +26,32 @@ def mkdir(name, clean=True):
             print(f"Removing {fn} from {name}")
             os.unlink(os.path.join(name,fn))
 
+def filestats(path):
+    """Compute the average FITS file size in MB and average flag file length in lines for
+       all FITS files and flag files in `path`
+
+       Returns
+       -------
+            tuple of (# FITS files, FITS size in MB, number of lines)
+    """
+    if path.is_file():
+        pass
+    if path.is_dir():
+        # get the FITS size 
+        nsize = []
+        for f in path.glob("*.fits"):
+            fstats = os.stat(f)
+            nsize.append(fstats.st_size)
+
+        # get the lines in file
+        nlines = []
+        for f in path.glob("*.flag"):
+            with open(f,'rb') as fp:
+                nlines.append(sum(1 for _ in fp))
+
+        meandata = np.mean(nsize)*u.byte
+        meanlines = int(np.mean(nlines))
+        return (len(nsize), meandata.to(u.megabyte).value, meanlines)
 
 
 progname  = "bench_gbtfitsload"
@@ -32,7 +60,7 @@ benchname = "GBTFITSLoad"
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog=progname)
     parser.add_argument("--dobench",     "-d", action="store_true",  help="do the benchmark test")
-    parser.add_argument("--key",         "-k", action="store",       help="input dysh_data key. ", default="multismall")
+    parser.add_argument("--key",         "-k", action="store",       help="input dysh_data key", default="multismallsmall")
     parser.add_argument("--numfiles",    "-n", action="store",       help="number of SDFITS files to load for multifile data", default=1)
     parser.add_argument("--loop",        "-l", action="store",       help="number of times to loop", default=4)
     parser.add_argument("--skipflags",   "-s", action="store_true",  help="skip reading flags")
@@ -40,7 +68,7 @@ if __name__ == "__main__":
     parser.add_argument("--append",      "-a", action="store_true",  help="append to previous output file (astropy Table)", required=False)
     parser.add_argument("--overwrite",   "-w", action="store_true",  help="overwrite a previous output file (astropy Table)", required=False)
     parser.add_argument("--profile",     "-p", action="store_true",  help="run the profiler")
-    parser.add_argument("--statslines",  "-e", action="store",       khelp="number of profiler statistics lines to print", default=25)
+    parser.add_argument("--statslines",  "-e", action="store",       help="number of profiler statistics lines to print", default=25)
     parser.add_argument("--quit",        "-q", action="store_true",  help="quit early")    
     #parser.add_argument("--noindex",     "-n", action="store_true",  help="do not create dysh index table (pandas)")
     args = parser.parse_args()
@@ -49,16 +77,14 @@ if __name__ == "__main__":
     if args.quit:
         sys.exit(0)
 
-    if args.nocalibrate and args.timeaverage:
-        raise Exception("You must calibrate if you want to time average")
-
     timestr = ""
     i = 0
     
     # output table colnames, units, and dtypes
-    table_cols = ["name", "time"]
+    table_cols = ["name", "time", "per file data (MB)", "flag (lines)", "skipflags"]
+    table_cols = ["name", "time", "skipflags"]
     table_units = []
-    table_dtypes = [str, int]
+    table_dtypes = [str, int, str]
     table = Table(names=table_cols, meta={"name": f"Dysh Benchmark {benchname} {args.key}"}, units=table_units, dtype=table_dtypes)
     if args.profile:
         pr = cProfile.Profile()
@@ -67,20 +93,25 @@ if __name__ == "__main__":
     time_stats = []
     time_data = []
     time_data.append(time.perf_counter_ns())
-    time_stats.append(["start", time_data[-1]])
+    sk = str(args.skipflags)
+    time_stats.append(["start", time_data[-1], ""])
 
-    f1 = dysh_data(acceptance=args.key)
+    f1 = dysh_data(accept=args.key)
     # use secret GBTFITSLOAD nfiles kwarg to limit number of files loaded.
     nfiles = int(args.numfiles)
-    print(f"Loading {nfiles} from {f1}")
-    if args.dotest:
+    print(f"Loading not more than {nfiles} from {f1}")
+    file_stats = filestats(f1)
+    print(f"Found {file_stats[0]} files, FITS size per file {np.round(file_stats[1],2)}MB, Flag lines {file_stats[2]}")
+    if args.dobench:
         for i in range(1,int(args.loop)+1):
             sdf1 = GBTFITSLoad(f1, skipflags=args.skipflags, nfiles=nfiles)
+            num_loaded = len(sdf1.files)
             time_data.append(time.perf_counter_ns())
-            time_stats.append([f"load{i}-{nfiles}", time_data[-1]])
+            k = f"load{i}-{num_loaded}"
+            time_stats.append([k, time_data[-1], sk])
 
     time_data.append(time.perf_counter_ns())
-    time_stats.append(["end", time_data[-1]])
+    time_stats.append(["end", time_data[-1], ""])
     print((time_stats[-1][1]-time_stats[0][1])/1e9)
         
     if args.profile:
