40e09d5579f9f2474f159245b1a1df0e38ed96a3
Peter Teuben <teuben@gmail.com>
2025-04-21 15:40:28 -0400

    use the new DTime() approach

--- benchmark/bench_getps.py
+++ benchmark/bench_getps.py
@@ -12,6 +12,7 @@ import numpy as np
 from astropy.table import Table,vstack
 from dysh.fits.gbtfitsload import GBTFITSLoad
 from dysh.util.files import dysh_data
+from dysh.util.timers import DTime
 
 def mkdir(name, clean=True):
     """ simpler frontend for making a directory that might also already exist
@@ -43,7 +44,7 @@ if __name__ == "__main__":
     parser.add_argument("--profile",     "-p", action="store_true",  help="run the profiler")
     parser.add_argument("--statslines",  "-e", action="store",      help="number of profiler statistics lines to print", default=25)
     parser.add_argument("--quit",        "-q", action="store_true",  help="quit early")    
-    parser.add_argument("--justtable",   "-j", action="store_true",  help="just print the existin table and exit")    
+    parser.add_argument("--justtable",   "-j", action="store_true",  help="just print the existing table and exit")    
     #parser.add_argument("--index", "-i", action="store_true", help="create dysh index table (pandas)")
     args = parser.parse_args()
     print(f"using {args}")
@@ -63,48 +64,36 @@ if __name__ == "__main__":
 
     timestr = ""
     i = 0
-    
-    # output table colnames, units, and dtypes
-    table_cols = ["name", "time", "skipflags"]
-    table_units = ["","ms",""]
-    table_dtypes = [str, float, str]
-    table = Table(names=table_cols, meta={"name": f"Dysh Benchmark {benchname}"}, units=table_units, dtype=table_dtypes)
+
     if args.profile:
         pr = cProfile.Profile()
         pr.enable()
+
         
-    time_stats = []
-    time_data = []
-    time_data.append(time.perf_counter_ns())
-    #time_stats.append(["start", time_data[-1]])
-    time_stats.append(["start",  0, ""])
+    data_cols  = ["skipflags"]
+    data_units = [""]
+    data_types = [str]
+    dt = DTime(out=args.out,
+               data_cols=data_cols, data_units=data_units, data_types=data_types)  # no data=[] supported yet
+
+    sk=str(args.skipflags)
 
     f1 = dysh_data(example=args.key)     # 'test1' = position switch example from notebooks/examples
     print("Loading ",f1)
     sdf1 = GBTFITSLoad(f1, skipflags=args.skipflags)
-    time_data.append(time.perf_counter_ns())
-    sk=str(args.skipflags)
-    time_stats.append(["load", (time_data[-1]-time_data[-2])/1E6, sk])
+    dt.tag("load", [sk])
     calibrate = not args.nocalibrate
     if args.dobench:
         #scans = [51,53,55,57]
         scans = [51]
         for i in range(1,int(args.loop)+1):
             sb = sdf1.getps(scan=scans, fdnum=0, ifnum=0, plnum=0, calibrate=calibrate)
-            time_data.append(time.perf_counter_ns())
-            #time_stats.append([f"getps{i}s", time_data[-1]])
-            time_stats.append([f"getps{i}s", (time_data[-1]-time_data[-2])/1E6, sk])
+            dt.tag(f"getps{i}s", [sk])
             #ps1 = sdf1.getps(scan=scans, fdnum=0, ifnum=0, plnum=0).timeaverage()
             if args.timeaverage:
                 ps = sb.timeaverage()
-                time_data.append(time.perf_counter_ns())
-                #time_stats.append([f"getps{i}t", time_data[-1]])
-                time_stats.append([f"getps{i}t", (time_data[-1]-time_data[-2])/1E6, sk])
-      
-
-    time_data.append(time.perf_counter_ns())
-    time_stats.append(["end-start", (time_data[-1]-time_data[0])/1E6,""])
-    #print((time_stats[-1][1]-time_stats[0][1])/1e9)
+                dt.tag(f"getps{i}t",[sk])
+    dt.tag("done",[sk])
         
     if args.profile:
         pr.disable()
@@ -112,35 +101,12 @@ if __name__ == "__main__":
         #ps = pstats.Stats(pr).sort_stats(SortKey.CUMULATIVE, SortKey.TIME)
         ps.print_stats(int(args.statslines))
 
-    for i in range(len(time_stats)):
-        table.add_row(time_stats[i])
+    dt.tag('report',[sk])
+    dt.close()
+    dt.report()
 
-    for c in table.columns:
-        if table[c].info.dtype == float:
-            table[c].info.format = "0.1f"
-
-    if args.out is not None:
-        if os.path.exists(args.out):
-            if args.append:
-                oldtable = Table.read(args.out, format="ipac")
-                table2 = vstack([oldtable, table])
-            elif args.overwrite:
-                table2 = table
-            else:
-                raise Exception(f"{args.out} exists. Use -w to overwrite.")
-        else:
-            table2 = table
-        table2.write(args.out, format="ipac", overwrite=True)
-    else:
-        table.pprint_all()
-
-
-    # one final time?
-    time_data.append(time.perf_counter_ns())
-    print('final',(time_data[-1]-time_data[0])/1e9,'sec')
-
-    if False:
-        for i in range(1,len(time_stats)):
-            dt = (time_stats[i][1]-time_stats[i-1][1])/1e6
-            label = time_stats[i][0]
-            print(label, dt)
+    # report total CPU time in sec.
+    # This does not include startup time before the DTime() [~2 sec]
+    total = dt.total()
+    print('final',total/1000,' sec')
+    
