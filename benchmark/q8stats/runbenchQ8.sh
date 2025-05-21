f6d1d50315ea3e7cacc2e8cc27e689e8d99e43fe
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-05-20 20:16:00 -0400

    add SB write bench

--- benchmark/q8stats/runbenchQ8.sh
+++ benchmark/q8stats/runbenchQ8.sh
@@ -12,21 +12,19 @@ nfile=(8 8 5 3)
 #####################
 
 # skip gbtfitsload because i already ran this bench
-if false; then
-    for i in $(seq 0 3)
-    do
-        out="benchtest$i.tab"
-        opr="${dd[$i]}.profile.time"
-        oprs="${dd[$i]}.profile.skipflags.time"
-        #for j in $(seq 1 ${nfile[$i]})
-        j=${nfile[$i]}
-        #do
-            ../bench_gbtfitsload.py -d -l 4 -n $j  -k ${dd[$i]} --statslines 50 -m -p  -x time > $opr
-            ../bench_gbtfitsload.py -d -l 4 -n $j -s -k ${dd[$i]}  --statslines 50 -m  -p  -x time > $oprs
-            echo "done ${dd[$i]}"
-        #done
-    done
-fi
+for i in $(seq 0 3)
+do
+    out="benchtest$i.tab"
+    opr="${dd[$i]}.profile.time"
+    oprs="${dd[$i]}.profile.skipflags.time"
+    #for j in $(seq 1 ${nfile[$i]})
+    j=${nfile[$i]}
+    #do
+        ../bench_gbtfitsload.py -d -l 4 -n $j  -k ${dd[$i]} --statslines 50 -m -p  -x time > $opr
+        ../bench_gbtfitsload.py -d -l 4 -n $j -s -k ${dd[$i]}  --statslines 50 -m  -p  -x time > $oprs
+        echo "done ${dd[$i]}"
+    #done
+done
 
 #####################
 # GETPS
@@ -37,4 +35,9 @@ echo "doing GETPS bench..."
 ../bench_getps.py -d -s --statslines 50 -m -p -x time > getps_bench.profile.time
 ../bench_getps.py -d -s --statslines 50 -m -p > getps_bench.profile
 
+#####################
+# SCAN WRITE
+#####################
+echo "doing Scanblock write bench..."
+../bench_datawrite.py --writedata sb -l 10 --statslines 25 -m -p > sbwrite_bench.profile
 exit
