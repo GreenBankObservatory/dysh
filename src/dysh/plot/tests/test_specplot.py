5df676a6630cd3d9e7472335f457c8bcde938dab
etsmit <etsmit12@gmail.com>
2025-06-25 15:26:43 -0400

    fix pytest to accept savefig change

--- src/dysh/plot/tests/test_specplot.py
+++ src/dysh/plot/tests/test_specplot.py
@@ -20,12 +20,12 @@ class TestSpecplot:
         p = get_project_testdata()
         sdf = GBTFITSLoad(p / "AGBT20B_014_03.raw.vegas/AGBT20B_014_03.raw.vegas.A6.fits")
         tp = sdf.gettp(scan=6, plnum=0, ifnum=0, fdnum=0).timeaverage()
-        tp.plot(show=False)
+        tpplot = tp.plot(interactive=False)
         of = tmp_path / "test_savefig.png"
-        tp.savefig(of)
+        tpplot.savefig(of)
         assert of.is_file()
 
-        tp.plot(show_header=False, show=False)
+        tpplot = tp.plot(show_header=False, interactive=False)
         of = tmp_path / "test_savefig_noheader.png"
-        tp.savefig(of)
+        tpplot.savefig(of)
         assert of.is_file()
