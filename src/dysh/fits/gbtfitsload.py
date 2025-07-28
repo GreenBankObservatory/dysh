84178ca23332228fbdcc2b8806b89c8f4cd5274a
astrofle <psalas@nrao.edu>
2025-07-28 16:07:03 -0400

    fix: pre-commit ruff

--- src/dysh/fits/gbtfitsload.py
+++ src/dysh/fits/gbtfitsload.py
@@ -1963,7 +1963,7 @@ class GBTFITSLoad(SDFITSLoad, HistoricalBase):
         smoothref: int = 1,
         apply_flags: bool = True,
         bunit="ta",
-        zenith_opacity: float = None,
+        zenith_opacity=None,
         t_sys=None,
         **kwargs,
     ):
@@ -2040,7 +2040,6 @@ class GBTFITSLoad(SDFITSLoad, HistoricalBase):
                 for scan in scans:
                     reftp = []
                     sigtp = []
-                    fulltp = []
                     logger.debug(f"doing scan {scan}")
                     df = select_from("SCAN", scan, _df)
                     if len(df) == 0:
