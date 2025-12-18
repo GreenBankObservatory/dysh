9668b66a7add0f1fff11be49d7f5c3aa3a13036d
Marc Pound <22331890+mpound@users.noreply.github.com>
2025-12-18 14:23:09 -0800

    update gbtlines to include atomic lines (HI was missing!)

--- scripts/makegbtline.py
+++ scripts/makegbtline.py
@@ -36,7 +36,7 @@ parser.add_argument(
 # this is the original line from grom GBTIDL. We use this to get the names of the molecules to search for.
 gbtidl_file = get_project_data() / "GBTIDL_RRF_w_Kup.csv.gz"
 # add $ to regex to eliminate, e.g., "Carbon Monoxide Ion" as a match
-redshift_lines = ["Carbon Monoxide$", "Atomic Hydrogen", "Atomic Carbon", "Atomic Oxygen", "Carbon Monosulfide$", r"HCO\+", " HCN "]
+redshift_lines = ["Carbon Monoxide$", "Atomic Hydrogen", "Atomic Carbon", "Atomic Oxygen", "Atomic Helium", "Atomic Deuterium", "Carbon Monosulfide$", r"HCO\+", " HCN "]
 redshift_recomb_lines = ["Recombination"]
 
 args = parser.parse_args()
