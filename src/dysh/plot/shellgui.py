ea09d93b7d7e2714d1a9f9f6fd4baec17b9bd41a
astrofle <psalas@nrao.edu>
2026-02-04 16:09:46 -0500

    plotter refactoring
    Implements a BaseGUI class to handle common GUI components.
    Changes behavior of replot or show.
    Changes order of tests to avoid issues due to interaction of shell
    tests and plotting.
    Adds StaticLabGUI for RtD rendering of notebooks.

--- src/dysh/plot/shellgui.py
+++ src/dysh/plot/shellgui.py
@@ -2,19 +2,29 @@
 Graphical User Interface for dysh shell.
 """
 
-import tkinter as tk
-from tkinter import ttk
+import os
 
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 
 from ..util.core import in_notebook
+from .basegui import BaseGUI
 
-if not in_notebook():
+no_display = os.environ.get("DISPLAY", "") == ""
+if not no_display:
+    import tkinter as tk
+    from tkinter import ttk
+
+if not in_notebook() and not no_display:
     root = tk.Tk()
     root.withdraw()
 
 
-class ShellGUI:
+class ShellGUI(BaseGUI):
+    """
+    Tk based GUI.
+    Used for plotting in the shell.
+    """
+
     def __init__(self, plotbase):
         self.root = tk.Toplevel()
         self.root.geometry("900x700")
@@ -22,12 +32,12 @@ class ShellGUI:
         self.root.columnconfigure(0, weight=1)
         self.root.rowconfigure(0, weight=1)
         self.root.rowconfigure(1, weight=1)
+        self.bframe = ttk.Frame(self.root, width=900, height=100)  # Button frame.
+        self.bframe.pack(expand=True, fill=tk.BOTH, side=tk.TOP)
         self.frame = ttk.Frame(self.root, width=900, height=300)
         self.frame.pack(expand=True, fill=tk.BOTH, side=tk.TOP)
         self.frame.columnconfigure(0, weight=1)
-        self.frame.columnconfigure(1, weight=1)
         self.frame.rowconfigure(0, weight=1)
-        self.frame.rowconfigure(1, weight=1)
         self.canvas = FigureCanvasTkAgg(plotbase.figure, master=self.frame)
         # Using a manager causes things to freeze!
         # self.manager = FigureManagerTk(self.canvas, 0, self.root)
@@ -35,17 +45,24 @@ class ShellGUI:
         self.toolbar.update()
         self.canvas.draw()
         self.toolbar.pack(side=tk.BOTTOM, fill=tk.X, expand=True)
-        self.canvas.get_tk_widget().grid(column=0, row=1, columnspan=10, sticky="nsew")
+        self.canvas.get_tk_widget().grid(column=0, row=0, sticky="nsew")
 
-        if hasattr(plotbase, "_selector"):
-            self.button_clear = ttk.Button(
-                master=self.frame, text="Clear All Regions", command=plotbase._selector.clear_regions
-            )
-            self.button_clear.grid(column=0, row=0)
-            self.button_clear_one = ttk.Button(
-                master=self.frame, text="Clear Region", command=plotbase._selector.clear_region
-            )
-            self.button_clear_one.grid(column=1, row=0)
+        self.button_clear = ttk.Button(master=self.bframe, text="Clear All Regions")
+        self.button_clear.grid(column=0, row=0)
+        self.button_clear_one = ttk.Button(master=self.bframe, text="Clear Region")
+        self.button_clear_one.grid(column=1, row=0)
+
+    def connect_buttons(self, plotbase):
+        if plotbase.has_selector():
+            self.button_clear.bind("<Button>", plotbase._selector.clear_regions)
+            self.button_clear_one.bind("<Button>", plotbase._selector.clear_region)
+
+    def disconnect_buttons(self):
+        self.button_clear.unbind("<Button>")
+        self.button_clear_one.unbind("<Button>")
+
+    def is_window_alive(self):
+        return bool(self.root.winfo_exists())
 
     def show(self):
         return
