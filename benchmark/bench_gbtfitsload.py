68acdcae9a5376f25bc9b7e5a5f998936b9bc376
Marc Pound <22331890+mpound@users.noreply.github.com>
2026-02-19 11:06:43 -0800

    additional logging

--- benchmark/bench_gbtfitsload.py
+++ benchmark/bench_gbtfitsload.py
@@ -11,6 +11,7 @@ import numpy as np
 from dysh.fits.gbtfitsload import GBTFITSLoad
 from dysh.util.files import dysh_data
 from dysh.util.timers import DTime
+from dysh.log import init_logging
 
 
 def mkdir(name, clean=True):
@@ -94,7 +95,7 @@ if __name__ == "__main__":
 
     if args.quit:
         sys.exit(0)
-
+    init_logging(3)
     # output table colnames, units, and dtypes
     # DTime automatically handles name and time, so just the additional columns go here.
     data_cols = ["#files", "file_size", "totsize", "nchan", "nrow", "nIF", "nFd", "nPol", "#flags", "skipflags"]
